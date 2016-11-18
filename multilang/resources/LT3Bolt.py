b503c440fe3caca3e1f65e9ff2cfd4b8c5accdaf
sborny <sander.borny@ugent.be>
2016-11-18 09:59:27 +0100

    Remove print statements

--- multilang/resources/LT3Bolt.py
+++ multilang/resources/LT3Bolt.py
@@ -4,6 +4,8 @@ import json as simplejson
 sys.path.append('/tmp/Amica/LT3/amica_demo/suicide')
 from pipeline import suicidality_pipeline as sp
 
+#Decorator to catch printed output from sp.get_result(text)
+#get_result() should always print the json message before return statement
 def capture(f):
     def captured(*args, **kwargs):
         import sys
@@ -14,12 +16,12 @@ def capture(f):
         try:
             sys.stdout = StringIO()  
             f(*args, **kwargs)
-            out = sys.stdout.getvalue() # release output
+            out = sys.stdout.getvalue()
         finally:
-            sys.stdout.close()  # close the stream 
-            sys.stdout = backup # restore original stdout
+            sys.stdout.close()  
+            sys.stdout = backup 
 
-        return out # captured output wrapped in a string
+        return out 
 
     return captured
 
@@ -27,9 +29,7 @@ def capture(f):
 def get_res(text):
     return sp.get_result(text)
 
-class LT3Bolt(storm.BasicBolt):
-
-	
+class LT3Bolt(storm.BasicBolt):	
 
     def initialize(self, conf, context):
         self._conf = conf;
@@ -37,44 +37,29 @@ class LT3Bolt(storm.BasicBolt):
         storm.logInfo("LT3Bolt instance starting ...")
 
     def process(self, tuple):
-        storm.logInfo("LT3Bolt tuple incomming")
         id_tweet, text = tuple.values
         storm.logInfo(text)
-        #json = sp.get_result(text)
+
         json = get_res(text)
         ''.join(json)
-        #print '[%s]' % ', '.join(map(str, json_string))
         json = json.split('\n')[-2]        
         json_string = json.replace("'", '"')
-        storm.logInfo("JSON")
-        storm.logInfo(json_string)
         
         data = simplejson.loads(json_string)
-        storm.logInfo(data)
-        storm.logInfo("JSON OBJECT SUCCES")
         data['id'] = str(id_tweet)
-        storm.logInfo(data)
         data['source'] = "LT3"
-        storm.logInfo(data)
 
         if(data['relevance_boolean'] == 1 and data['severity_boolean'] == 1):
-            data['flag'] = "LT3"
-            storm.logInfo("JSON 1 and 1")            
+            data['flag'] = "LT3"           
         else:
             data['flag'] = "none"
-            storm.logInfo("JSON OBJECT none")
             
         del data['relevance_boolean'] 
         del data['severity_boolean']
-        storm.logInfo("JSON OBJECT DEL")
 
         json_string = simplejson.dumps(data)
         
-        storm.logInfo("LT3Bolt emitting tuple")
-        storm.emit([json_string])
-
-
-         
+        storm.emit([json_string])         
 
 
 LT3Bolt().run()
